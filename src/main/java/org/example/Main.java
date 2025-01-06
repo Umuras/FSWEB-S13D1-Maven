8cf892ddc007cbdccfb098d37e34c527ff368c98
Ali Umur Kucur <aliumurkucur@gmail.com>
2025-01-06 13:22:35 +0300

    All tests finished.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,26 +2,73 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(shouldWakeUp(true,1));
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(5.0));
+
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if(isBarking && clock >= 0 && clock <=23) {
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       if(firstAge >= 13 && firstAge <= 19)
+       {
+           return true;
+       } else if(secondAge >= 13 && secondAge <= 19)
+       {
+           return true;
+       } else if(thirdAge >= 13 && thirdAge <= 19)
+       {
+           return true;
+       } else {
+           return false;
+       }
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+
+      if(isSummer)
+      {
+          if(temp >= 25 && temp <= 45)
+          {
+              return true;
+          }else{
+              return false;
+          }
+      }else {
+          if(temp >= 25 && temp <= 35)
+          {
+              return true;
+          }else{
+              return false;
+          }
+      }
     }
 
     public static double area(double width, double height) {
-       
+       if(width < 0 || height < 0)
+       {
+           return -1;
+       }
+
+       return width * height;
     }
 
     public static double area(double radius) {
-       
+       if(radius < 0)
+       {
+           return -1;
+       }
+
+       return radius*radius*Math.PI;
     }
 }
